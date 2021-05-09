b1a530c33362ed3072960e2f53fea463238c1399
mamchura11072000 <57662348+mamchura11072000@users.noreply.github.com>
2021-05-10 01:51:43 +0300

    Commit1 helper.php index.html mod_ame.php mod_ame.xml tmpl/default.php tmpl/index.html language/ru-RU/ru-RU.mod_ame.ini language/ru-RU/ru-RU/ru-RU/mod_ame.sys.ini

---
+++ helper.php
@@ -0,0 +1,13 @@
+<?php
+ /**
+ * @package ame_admin
+ * @author Alexandr Mamchur (mamchur.a@bk.ru)
+ * @version 1.0.0
+ * @copyright (C) 2016 by JoomLine (http://www.joomline.net)
+ * @license GNU/GPL: http://www.gnu.org/copyleft/gpl.html
+ *
+*/
+
+// no direct access
+defined('_JEXEC') or die;
+
